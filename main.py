232a3667d0498eb909ea540c4045053f6fe3e425
rahulraju3107 <rahulraju3107@gmail.com>
2025-08-14 11:48:55 +1000

    modified main

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        contents = f.read()
+
+    return contents #string
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+if __name__ == "__main__":
+    main()
