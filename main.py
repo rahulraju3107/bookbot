7621bf21c8b22c3b349595743edfc02f37bbd044
rahulraju3107 <rahulraju3107@gmail.com>
2025-08-14 12:33:43 +1000

    add stats.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import get_number_of_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         contents = f.read()
     return contents
-
-def get_number_of_words(contents):
-    return len(contents.split())
 
 def main():
     num_words = get_number_of_words(get_book_text("books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_number_of_words(contents):
+    return len(contents.split())
