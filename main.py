b2df1108397a8cf5adad92777efb6fe88235df17
rahulraju3107 <rahulraju3107@gmail.com>
2025-08-16 13:50:44 +1000

    add sort function, updated report

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from stats import get_number_of_words, get_character_count
+from stats import get_number_of_words, get_character_count, sorted_list
 
+# Reads and returns contents of text file
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         contents = f.read()
     return contents
 
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    book = "books/frankenstein.txt"
+    book_contents = get_book_text(book)
+
+    # Get stats
     num_words = get_number_of_words(book_contents)
     num_chars = get_character_count(book_contents)
-    
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    sorted_chars = sorted_list(num_chars)
+
+    # Print report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in sorted_chars:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,12 +1,26 @@
 def get_number_of_words(contents):
     return len(contents.split())
 
+# Counts number of characters in text
 def get_character_count(contents):
     counts = {}
     for ch in contents:
         key = ch.lower()
-        if key in counts:
-            counts[key] += 1
-        else:
-            counts[key] = 1
+        counts[key] = counts.get(key, 0) + 1
     return counts
+
+# Sort by frequency
+def sorted_list(counts):
+    rows = []
+    for char, freq in counts.items():
+        if not char.isalpha():
+            continue
+        rows.append({"char": char, "num": freq})
+    
+    # Helper function for sorting
+    def sort_on(item):
+        return item["num"]
+    
+    rows.sort(reverse=True, key=sort_on)
+
+    return rows
